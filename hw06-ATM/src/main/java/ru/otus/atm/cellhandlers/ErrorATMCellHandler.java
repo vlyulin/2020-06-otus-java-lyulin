8f90e068b9085cd9d6193b49a46803da37d18a1e
Vadim Lyulin <vlyulin@gmail.com>
2020-08-27 19:31:59 +0300

    hw06-ATM (#7)
    * hw06-ATM
    
    * hw06-ATM
    
    Co-authored-by: VLulin <vlulin@borlas.ru>

---
+++ hw06-ATM/src/main/java/ru/otus/atm/cellhandlers/ErrorATMCellHandler.java
@@ -0,0 +1,20 @@
+package ru.otus.atm.cellhandlers;
+
+import ru.otus.atm.commands.Command;
+import ru.otus.banknotes.Banknote;
+import ru.otus.atm.exceptions.ATMNotEnoughMoneyException;
+
+import java.util.Map;
+import java.util.Stack;
+
+/*
+ * Конечный обработчик, если запрашиваемая сумма не может быть выдана
+ */
+public class ErrorATMCellHandler extends BaseATMCellHandler {
+
+    @Override
+    public void withdrawCash(Map<Banknote, Integer> banknotes, int amount) {
+        throw new ATMNotEnoughMoneyException("Запрашиваемая сумма "+amount+" не может быть выдана.");
+    }
+
+}

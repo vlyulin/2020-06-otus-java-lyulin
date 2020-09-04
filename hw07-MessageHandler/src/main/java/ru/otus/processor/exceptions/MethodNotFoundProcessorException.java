9268dafabec0768ff4661421401549ca0ac400bb
VLulin <vlulin@borlas.ru>
2020-09-04 21:08:02 +0300

    Move hw07-MessageHandler to proper branch

--- hw07-MessageHandler/src/main/java/ru/otus/processor/exceptions/MethodNotFoundProcessorException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.processor.exceptions;
-
-public class MethodNotFoundProcessorException extends RuntimeException {
-    public MethodNotFoundProcessorException(String message) {
-        super(message);
-    }
-}
--- hw07-MessageHandler/src/main/java/ru/otus/processor/exceptions/ProxyLoggerException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.processor.exceptions;
-
-public class ProxyLoggerException extends ProcessorException {
-    public ProxyLoggerException(String message) {
-        super(message);
-    }
-}

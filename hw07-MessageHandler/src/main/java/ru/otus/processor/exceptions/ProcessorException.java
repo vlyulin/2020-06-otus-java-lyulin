29d281e2440f7b6a8739a373e3aa9f94c5f19e5e
VLulin <vlulin@borlas.ru>
2020-09-04 21:28:01 +0300

    hw07-MessageHandler

--- hw07-MessageHandler/src/main/java/ru/otus/processor/exceptions/ProcessorException.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.processor.exceptions;
-
-public class ProcessorException extends RuntimeException {
-    public ProcessorException(String message) {
-        super(message);
-    }
-}

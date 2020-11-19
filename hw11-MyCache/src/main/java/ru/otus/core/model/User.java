4287b9a732deb79d45ad3e196888179baf21ff48
Vadim Lyulin <vlyulin@gmail.com>
2020-11-19 21:04:41 +0300

    hw10-Hibernate (#12)
    * hw10-Hibernate
    
    * Add build.gradle
    
    Co-authored-by: VLulin <vlulin@borlas.ru>

---
+++ hw11-MyCache/src/main/java/ru/otus/core/model/User.java
@@ -0,0 +1,107 @@
+package ru.otus.core.model;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * @author sergey
+ * created on 03.02.19.
+ */
+@NamedEntityGraph(
+        name = "user-entity-graph",
+        attributeNodes = {
+                @NamedAttributeNode("phones"),
+                @NamedAttributeNode("addresses"),
+        }
+)
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", insertable=false, updatable=false)
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = PhoneDataSet.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private Set<PhoneDataSet> phones;
+
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = AddressDataSet.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private Set<AddressDataSet> addresses;
+
+    public User() {
+    }
+
+    public User(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public Set<PhoneDataSet> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(Set<PhoneDataSet> phones) {
+        this.phones = phones;
+    }
+
+    public Set<AddressDataSet> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<AddressDataSet> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id &&
+                age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
---
+++ hw11-MyCache/src/main/java/ru/otus/core/service/DbServiceUserImpl.java
@@ -0,0 +1,57 @@
+package ru.otus.core.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.core.dao.UserDao;
+import ru.otus.core.model.User;
+import ru.otus.core.sessionmanager.DatabaseSession;
+import ru.otus.hibernate.sessionmanager.SessionManagerHibernate;
+
+import java.util.Optional;
+
+public class DbServiceUserImpl implements DBServiceUser {
+    private static final Logger logger = LoggerFactory.getLogger(DbServiceUserImpl.class);
+    private final UserDao userDao;
+
+    public DbServiceUserImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    public long saveUser(User user) {
+        try (var sessionManager = userDao.getSessionManager()) {
+//            sessionManager.beginSession();
+            try {
+                var userId = userDao.insertUser(user);
+                // В Java борятся за миллисекунды, но допускается делать commit после каждой вставки?
+                // Это "дорогая" операция и в базах рекомендуют делать как можно реже и только когда действительно надо
+                // При текущем подходе возникает вопрос, как откатить все изменения,
+                // если где-то посередине транзакции что-то пошло не так?
+//                sessionManager.commitSession();
+                logger.info("created user: {}", userId);
+                return userId;
+            } catch (Exception e) {
+//                sessionManager.rollbackSession();
+                throw new DbServiceException(e);
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> getUser(long id) {
+        try (var sessionManager = userDao.getSessionManager()) {
+            // Вызов beginSession тут дает непредсказуемость поведения
+            // По моему мнению, сессии должны управляться вызывающим методом
+//            sessionManager.beginSession();
+            try {
+                Optional<User> userOptional = userDao.findById(id);
+                logger.info("user: {}", userOptional.orElse(null));
+                return userOptional;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+//                sessionManager.rollbackSession();
+            }
+            return Optional.empty();
+        }
+    }
+}

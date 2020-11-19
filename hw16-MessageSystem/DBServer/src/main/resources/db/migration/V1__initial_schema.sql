e14f605ac4d4e3b8199d1893f2fb90b042d67c17
Vadim Lyulin <vlyulin@gmail.com>
2020-11-19 20:59:32 +0300

    Hw16 message system (#17)
    * hw15-Executors
    
    * add hw16-MessageSystem
    
    * hw16-MessageSystem
    
    * Add MessageBuilder
    
    * Add MessageBuilder
    
    Co-authored-by: VLulin <vlulin@borlas.ru>
    Co-authored-by: Vadim Lyulin <borlas_Vadim.Lyulin@eurochem.ru>

---
+++ hw16-MessageSystem/DBServer/src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,19 @@
+create table users(
+    id bigint(20) not null,
+    name varchar2(255),
+    age int(3),
+    login varchar2(30) not null,
+    password varchar2(30) not null
+    );
+create table Phones(
+    id bigint(20) not null,
+    user_id bigint(20),
+    phone_number varchar2(255)
+    );
+create table Addresses(
+    id bigint(20) not null,
+    user_id bigint(20),
+    street varchar2(255)
+    );
+create sequence hibernate_sequence start with 1 increment by 1;
+
